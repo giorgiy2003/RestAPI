7a0630c9d5610e7b51f66b4afd5d2e028f5b1116
Giorgiy Baroyan <giorgiy_03@mail.ru>
2022-11-03 15:03:04 +0300

    corrective

--- RestAPIPostgres-Echo/RestAPIwithPostgres/internal/handlers/Person_handlers.go
+++ RestAPIPostgres-Echo/RestAPIwithPostgres/internal/handlers/Person_handlers.go
@@ -95,7 +95,7 @@ func check(ctx context.Context, errorCh chan error) {
 		Logic.Log.Fatalf("Timed out: %v", ctx.Err())
 		return
 	case err := <-errorCh:
-			Logic.Log.Errorf("Возникла ошибка... %v", err)
-			return
+		Logic.Log.Errorf("Возникла ошибка... %v", err)
+		return
 	}
 }
